86b876a2f78165f1da52f1ef75d60d970f20c5d5
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-09-21 08:29:57 +0200

    Proves GIT

---
+++ UF1/A1-estructura-programa/Git/comprovarEdat.py
@@ -0,0 +1,14 @@
+"""
+Pol Cerrillo
+21/09/2023
+ASIXc1C M03 UF1
+Descripció: Exemple dels apunts
+"""
+
+#Programa que demana l'edat i diu si ets major d'edat.
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+
+print("Programa Finalitzat")
+
