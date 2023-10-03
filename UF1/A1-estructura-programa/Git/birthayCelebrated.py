2c013826d1ec75ce183635a04d9e5db67f766894
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-03 10:18:02 +0200

    Proves GIT

---
+++ UF1/A1-estructura-programa/Git/birthayCelebrated.py
@@ -0,0 +1,17 @@
+"""
+Pol Cerrillo
+03/10/2023
+ASIXc1C M03 UF1
+Descripció: Birthay Celebrated
+"""
+# Programa demana data de naixament i data actual
+
+diaAniversari = int(input("Quin dia vas neixer?"))
+mesAniversari = int(input("Quin mes vas neixer?"))
+diaActual = int(input("Quin dia es avui?"))
+mesActual = int(input("Quin mes es avui?"))
+
+if mesAniversari < mesActual:
+    print("Has celebrat el teu aniversari")
+else:
+    print("No has celebrat encara el teu aniversari")
