207671893b6853c88cccc1323f342657606689db
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-03 12:04:55 +0200

    Proves GIT

---
+++ UF1/A1-estructura-programa/Git/nextNumber.py
@@ -0,0 +1,11 @@
+"""
+Pol Cerrillo
+03/10/2023
+ASIXc1C M03 UF1
+Descripció: Next Number
+"""
+# Programa Printa per pantalla el número següent.
+
+n = int(input("Numero?"))
+numeroAfter = n+1
+print("Després ve el", numeroAfter)
