a18f85c33b1d228ec85e155e3daf6836db8762a2
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-19 08:06:40 +0200

    Proves GIT

---
+++ UF1/A2-Manipulacio-de-dades/Exercicis/E15.py
@@ -0,0 +1,12 @@
+"""
+Pol Cerrillo
+10/10/2023
+ASIXc1C M03 UF1
+Descripció:Escriu un programa que donat el nom i els dos cognoms introduïts per un
+usuari, mostri, només, les inicials corresponents en majúscules.
+"""
+nom=input("Nom: ")
+c1=input("1r cognom: ")
+c2=input("2n cognom: ")
+longitud=len()
+for letras in range(0):
