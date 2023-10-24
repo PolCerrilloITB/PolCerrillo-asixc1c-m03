6e1e5e1742d2e92d6f32e17868ab10add8ccd66c
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-24 12:11:45 +0200

    Proves GIT

---
+++ UF1/A3-Estructures-de-selecció/willWeFightForTheCookies.py
@@ -0,0 +1,12 @@
+"""
+Pol Cerrillo
+24/10/2023
+ASIXc1C M03 UF1
+Descripció: Galetes == Persones?
+"""
+
+persones, galetes = int(input())
+if persones == galetes or galetes*2 == persones:
+    print("Let's Eat!")
+else:
+    print("Let's Fight")
