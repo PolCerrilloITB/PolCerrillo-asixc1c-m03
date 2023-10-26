cd53d38cd0e05da700fee8ba6b4350a1cbd888c9
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-10-26 09:50:53 +0200

    Proves GIT

---
+++ UF1/A3-Estructures-de-selecció/minimumNotesAndCoins.py
@@ -0,0 +1,23 @@
+"""
+Pol Cerrillo
+24/10/2023
+ASIXc1C M03 UF1
+Descripció: Minim nombre de monedes
+"""
+total = float(input())
+c1 = 1
+c2 = 2
+c3 = 5
+c4 = 10
+c5 = 20
+c6 = 50
+m1 = 1
+m2 = 2
+b1 = 5
+b2 = 10
+b3 = 20
+b4 = 50
+b5 = 100
+b6 = 200
+b7 = 500
+if total > b7:
