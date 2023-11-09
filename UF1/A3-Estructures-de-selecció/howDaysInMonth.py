656df39db04be2ed53ecf0ca55249806743fd1df
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-09 09:29:32 +0100

    Proves GIT

--- UF1/A3-Estructures-de-selecció/howDaysInMonth.py
+++ UF1/A3-Estructures-de-selecció/howDaysInMonth.py
@@ -7,7 +7,7 @@ Descripció: Dies en un mes
 
 mes = input("Quin mes vols?")
 match mes:
-    case"1"|"3"|"5"|"7"|"8"|"10"|"11": print("Te 31 dias el mes", mes)
+    case"1"|"3"|"5"|"7"|"8"|"10"|"12": print("Te 31 dias el mes", mes)
     case"2": print("Te 28 o 29 dias el mes", mes)
     case"4"|"6"|"9"|"11": print("Te 30 dias el mes", mes)
     case _: print("Error")
