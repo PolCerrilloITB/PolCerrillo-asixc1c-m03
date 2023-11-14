7b7ff67f6b0046b82a8ac56343518cc235464df8
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-14 10:27:08 +0100

    Proves GIT

---
+++ UF1/A4-Estructura-de-repetició/giveMeFive.py
@@ -0,0 +1,14 @@
+"""
+Pol Cerrillo
+14/11/2023
+ASIXc1C M03 UF1
+Descripció: Dona un numero fins que sigui 5
+"""
+
+number=0
+
+while number!=5:
+    number = int(input("Give me five:"))
+    if number ==1234 and number!=5:
+        number=5
+print("Great thank you")
