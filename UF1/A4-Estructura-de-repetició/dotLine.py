a7f1d98b790f53fbeb928e8743133fcdf85f43f0
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-16 09:40:39 +0100

    Proves GIT

--- UF1/A4-Estructura-de-repetició/dotLine.py
+++ UF1/A4-Estructura-de-repetició/dotLine.py
@@ -1,3 +1,11 @@
+"""
+Pol Cerrillo
+16/11/2023
+ASIXc1C M03 UF1
+Descripció: Demana un enter a l'usuari, per saber quantes línies
+Demana un enter a l'usuari, per saber quants punts
+Imprimeix per pantalla tantes línies amb tants punts com l'usuari hagi indicat
+"""
 linies = int(input("Linies: "))
 punts = int(input("Punts: "))
 for i in range(linies):
