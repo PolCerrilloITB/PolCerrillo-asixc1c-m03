d873de347e9352e38fbf18bcf51bd8c5be4dfcd3
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-23 07:54:34 +0100

    Proves GIT

--- UF1/A4-Estructura-de-repetició/multiplyTableFull.py
+++ UF1/A4-Estructura-de-repetició/multiplyTableFull.py
@@ -4,8 +4,8 @@ Pol Cerrillo
 ASIXc1C M03 UF1
 Descripció: Volem mostrar per pantalla les taules de multiplicar.
 """
-for i in range(1,10):
-    for n in range(1,10):
+for i in range(1, 10):
+    for n in range(1, 10):
         if i*n <= 9:
             print("", i*n, end=" ")
         else:
