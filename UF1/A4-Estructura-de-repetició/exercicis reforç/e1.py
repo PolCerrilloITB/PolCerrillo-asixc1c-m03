e058d1b4801c20edf82cab658452484ec2f60609
Pol <pol.cerrillo.7e7@itb.cat>
2023-12-13 18:19:42 +0100

    update

---
+++ UF1/A4-Estructura-de-repetició/exercicis reforç/e1.py
@@ -0,0 +1,19 @@
+"""
+Pol Cerrillo
+13/12/2023
+ASIXc1C M03 UF1
+Descripció: Implementa un programa que demani un número i calculi el seu factorial .
+El factorial d'un nombre és el producte de tots els enters entre 1 i el mateix nombre
+i es representa pel nombre seguit d'un signe d'exclamació. Per exemple 5! = 1x2x3x4x5= 120)
+"""
+try:
+    num = int(input("Numero ?"))
+    if num < 0:
+        print("Un numero positiu, si us plau")
+    else:
+        factorial = 1
+        for i in range(1, num+1):
+            factorial *= i
+        print(num, "=", factorial)
+except:
+    print("Error")
