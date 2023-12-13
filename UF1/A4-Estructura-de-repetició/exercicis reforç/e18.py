f8ac0102cac092ca9443421461b74066e644e7cf
Pol <pol.cerrillo.7e7@itb.cat>
2023-12-13 19:45:06 +0100

    update

--- UF1/A4-Estructura-de-repetició/exercicis reforç/e18.py
+++ UF1/A4-Estructura-de-repetició/exercicis reforç/e18.py
@@ -4,11 +4,14 @@ Pol Cerrillo
 ASIXc1C M03 UF1
 Descripció: Fer un programa que mostri per pantalla un cronòmetre, indicant les hores, minuts i segons.
 """
+#Variables i la configuració de time
 import time
 nhora = 00
 nmin = 00
 nseg = 00
+#definir el limit de cada variable
 while nseg <= 59 and nmin <= 59 and nhora <= 23:
+    #repetir fins que arribi a un valor definit
     if nseg == 59:
         nseg = 0
         if nmin == 59:
@@ -17,5 +20,7 @@ while nseg <= 59 and nmin <= 59 and nhora <= 23:
             nmin = nmin + 1
     else:
         nseg = nseg + 1
+        #time.sleep velocitat del cronometre
     time.sleep(0.25)
+    #str().zfill(valor dels valors que vols)
     print(str(nhora).zfill(2), ":", str(nmin).zfill(2), ":", str(nseg).zfill(2))
