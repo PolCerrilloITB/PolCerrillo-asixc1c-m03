db8b43e1b2930216a4d6a52bd1ed367acdd535f6
Pol <pol.cerrillo.7e7@itb.cat>
2023-12-13 19:28:19 +0100

    update

--- UF1/A4-Estructura-de-repetició/exercicis reforç/e10.py
+++ UF1/A4-Estructura-de-repetició/exercicis reforç/e10.py
@@ -4,8 +4,10 @@ Pol Cerrillo
 ASIXc1C M03 UF1
 Descripció: Escriu un programa que mostri la taula de multiplicar dels números 1,2,3,4 i 5.
 """
+#Rerpetició de 5 cops de la taula de multiplicar
 for i in range(1, 6):
     print("Taula de multiplicar del", i, ":")
+    #Repetició de 10 cops de la taula de multiplicar
     for n in range(1, 11):
         resultat = i * n
         print(i, "x", n, "=", resultat)
