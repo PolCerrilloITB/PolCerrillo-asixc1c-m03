7712a532cbfaac38462fe462005d0c6921fbed54
Pol <pol.cerrillo.7e7@itb.cat>
2023-12-13 19:48:08 +0100

    update

--- UF1/A4-Estructura-de-repetició/exercicis reforç/e1.py
+++ UF1/A4-Estructura-de-repetició/exercicis reforç/e1.py
@@ -7,13 +7,16 @@ El factorial d'un nombre és el producte de tots els enters entre 1 i el mateix 
 i es representa pel nombre seguit d'un signe d'exclamació. Per exemple 5! = 1x2x3x4x5= 120)
 """
 try:
+    #Valor del numero que volem
     num = int(input("Numero ?"))
+    #condicio de que el numero sigui positiu
     if num < 0:
         print("Un numero positiu, si us plau")
     else:
-        factorial = 1
+        #Variable de factor del numero escollit i repetir el valor factor multiplicant per el numero
+        factor = 1
         for i in range(1, num+1):
-            factorial *= i
-        print(num,"!", "=", factorial)
+            factor *= i
+        print(num,"!", "=", factor)
 except:
     print("Error")
