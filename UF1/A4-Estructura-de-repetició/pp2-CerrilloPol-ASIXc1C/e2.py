3fd66820a91b19b13e023f7eaeaa63e2bf661d1e
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-12-14 09:43:15 +0100

    update

--- UF1/A4-Estructura-de-repetició/pp2-CerrilloPol-ASIXc1C/e2.py
+++ UF1/A4-Estructura-de-repetició/pp2-CerrilloPol-ASIXc1C/e2.py
@@ -10,7 +10,11 @@ try:
     caracter = str(input("Quin caracter vols remplaçar?"))
     caracterSubs = str(input("Quin es el caracter de substitució?"))
     #Substitució del caracter a cambiar per un altre
-    resultat = paraula.replace(caracter, caracterSubs)
+    if caracter in paraula:
+        #TODO fer que canvi el caracter seleccionat per el que volem substituir.
+        # AMb el paraula.replace(caracter, caracterSubs) no es pot fer
+        caracterSubs = paraula
+    resultat = caracterSubs
     print("Resultat:")
     #Resultat del canvi
     print(resultat)
