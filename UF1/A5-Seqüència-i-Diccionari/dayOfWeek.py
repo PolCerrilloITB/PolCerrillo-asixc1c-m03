88ed7bc3d586203ee8c9021e9a47bf72dcf797ae
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-01-11 09:40:50 +0100

    Proves GIT

---
+++ UF1/A5-Seqüència-i-Diccionari/dayOfWeek.py
@@ -0,0 +1,16 @@
+"""
+Pol Cerrillo
+11/01/2024
+ASIXc1C M03 UF1
+Descripció: Donat un enter, mostrar el dia de la setmana amb text (dilluns, dimarts, dimecres…),
+tenint en compte que dilluns és el 1. Els dies de la setmana es guarden en una llista.
+"""
+setmana = ["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"]
+try:
+    diaSetmna = int(input())
+    if diaSetmna > 0 and diaSetmna < 8:
+        print(setmana[diaSetmna-1])
+    else:
+        print("la setmana te 7 dias")
+except:
+    print("Un numero")
