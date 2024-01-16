feff8f1a06d367bde90131a722aeef35d6170799
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-01-16 12:09:08 +0100

    Proves GIT

---
+++ UF1/A5-Seqüència-i-Diccionari/swap.py
@@ -0,0 +1,10 @@
+"""
+Pol Cerrillo
+16/01/2024
+ASIXc1C M03 UF1
+Descripció: Donada una llista de nombres sencers, intercanvia el primer per l'últim element.
+"""
+import numpy
+num = [int(input())]
+for i in range(len(num)):
+    num = numpy.append(-1), numpy.append()
