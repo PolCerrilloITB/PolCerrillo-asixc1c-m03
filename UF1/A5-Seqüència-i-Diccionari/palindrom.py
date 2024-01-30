7de5ddd3d1acf32460090476fe1b5ac80fb3fea6
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-01-30 11:42:59 +0100

    Proves GIT

---
+++ UF1/A5-Seqüència-i-Diccionari/palindrom.py
@@ -0,0 +1,32 @@
+"""
+Pol Cerrillo
+30/01/2024
+ASIXc1C M03 UF1
+Descripció: Indica si una paraula o frase és palíndrom.
+"""
+try:
+    frase = input().lower()
+    fraseNormal = []
+    fraseReves = []
+    for x in frase:
+        if x != " " and x != "." and x != "," and x != "’":
+            if x == "à" or x == "á":
+                x = "a"
+            elif x == "è" or x == "é":
+                x = "e"
+            elif x == "í":
+                x = "i"
+            elif x == "ò" or x == "ó":
+                x = "o"
+            elif x == "ú":
+                x = "u"
+            fraseNormal.append(x)
+    fraseReves.append(fraseNormal[-1::-1])
+    print(fraseReves)
+    print(fraseNormal)
+    if fraseReves[0] == fraseNormal:
+        print("Es palindrom.")
+    else:
+        print("No es palindrom.")
+except:
+    print("Una paraula")
