29d83bdb38d3cf6169a804c41252875eca6d834a
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-01 09:43:11 +0100

    examen PP3

---
+++ UF1/pp3-cerrillopol-ASIXc1C/e3.py
@@ -0,0 +1,12 @@
+"""
+Pol Cerrillo
+01/02/2023
+ASIXc1C M03 UF1
+Descripció: Mida paraulas:
+Implementar un programa que faci les següents operacions:
+Crear una variable per emmagatzemar 666 paraules
+Mostrar tot el seu contingut.
+Omplir-la, pel teclat, amb paraules. No cal omplir-la del toto. Quan la paraula introduïda és "\q" es deixad'introduir paraules.
+Calcular la llargada maxima de les paraules introduides i mostra-la per la pantalla.
+Mostrar la tupla amb les apraules i mides corresponents
+"""
