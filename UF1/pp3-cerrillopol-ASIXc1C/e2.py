2b56ab5523574a3e84d7096d0588abad368c5460
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-01 09:32:28 +0100

    examen PP3

--- UF1/pp3-cerrillopol-ASIXc1C/e2.py
+++ UF1/pp3-cerrillopol-ASIXc1C/e2.py
@@ -3,6 +3,9 @@ Pol Cerrillo
 01/02/2023
 ASIXc1C M03 UF1
 Descripció:Encriptar frases:
+Fer un programa per llegir una frase pel teclat i, en acabar, mostri la frase encriptada.
+Per encriptar la frase, ha de fer servir el valor numeric de cada lletra. Caldrà escriure un punt "." per
+poder indentificar a on comença i acaba cada lletra.
 """
 paraula = input("Donem una frase: ")
 if len(paraula) > 3:
