731cbb7a818a01f0864cdbfe17c17463309f24f7
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-08 09:27:49 +0100

    Proves GIT

---
+++ UF2/A1 Disseny descendent i parametrització/Yakisoba.py
@@ -0,0 +1,121 @@
+from time import sleep
+
+
+
+# region Declaració de Funcions ----------
+
+def recopilar_ingredients():
+
+   print("Comprar al supermercat")
+
+   sleep(1)
+
+   print("Disposar-los sobre el marbre")
+
+   sleep(1)
+
+def cuinar_ingredient():
+    print("Preparar aigua")
+
+    sleep(1)
+
+    print("Escalfar aigua")
+
+    sleep(1)
+
+    print("Posar-hi sal")
+
+    sleep(1)
+
+    print("Bullir tallarines")
+
+    sleep(1)
+
+    print("Escórrer tallarines")
+
+    sleep(1)
+
+    print("Deixar-les preparades")
+
+    sleep(1)
+def cuinar_pastanagues():
+
+    print("Tallar pastanagues")
+
+    sleep(1)
+
+    print("Fregir pastanagues")
+
+    sleep(1)
+
+    print("Preparar paella per fregir")
+
+    sleep(1)
+
+    print("Rossejar pastanagues")
+
+    sleep(1)
+
+    print("Netejar oli de paella")
+
+    sleep(1)
+
+    print("Deixar pastanagues preparades")
+
+    sleep(1)
+def cuinar_cebes():
+
+    print("Tallar cebes")
+
+    sleep(1)
+
+    print("Fregir cebes")
+
+    sleep(1)
+
+    print("Preparar paella per fregir")
+
+    sleep(1)
+
+    print("Rossejar cebes")
+
+    sleep(1)
+
+    print("Netejar oli de paella")
+
+    sleep(1)
+
+    print("Deixar cebes preparades")
+def preparacio_final():
+
+    print("Barrejar els ingredients amb salsa yakitori")
+
+    sleep(1)
+
+    print("Saltar ingredients")
+
+    sleep(1)
+
+    print("Preparar paella per saltar")
+
+    sleep(1)
+
+    print("Cuinar remenant ingredients")
+
+    sleep(1)
+
+    print("Deixar llest per servir")
+# endregion
+
+
+
+# region INICI: Preparar fideus Yakisoba
+
+recopilar_ingredients()
+cuinar_ingredient()
+cuinar_pastanagues()
+cuinar_pastanagues()
+preparacio_final()
+
+# endregion
+
