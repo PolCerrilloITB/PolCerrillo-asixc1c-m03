084118219b9d5322581dfff14126c2e07944962d
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-03-05 11:53:35 +0100

    Proves GIT

--- UF2/A1 Disseny descendent i parametrització/weather.py
+++ UF2/A1 Disseny descendent i parametrització/weather.py
@@ -1,6 +1,6 @@
 import requests
 
-city = 'london'
+city = 'Barcelona'
 api_url = 'https://api.api-ninjas.com/v1/weather?city={}'.format(city)
 response = requests.get(api_url, headers={'X-Api-Key': 'vEFL5OMr512PLVGOJ/neqA==72TQmVsyLk8GEoL3'})
 if response.status_code == requests.codes.ok:
