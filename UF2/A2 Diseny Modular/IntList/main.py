3d79c14f47a550b21e41cb17c0a48195d66f66b4
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-03-07 08:28:40 +0100

    Proves GIT

---
+++ UF2/A2 Diseny Modular/IntList/main.py
@@ -0,0 +1,10 @@
+'''
+Crear el mòdul kernel.py
+Defineix, a dins del mòdul,  la funció readIntList que llegeixi per teclat i retorna una llista amb aquests enters.
+Els enters són introduïts amb un espai de separació. El valor -1 indica que ja no hi ha més enters a llegir.
+def readIntListFromKeyboard():
+   numbers=[]
+   #TODO: ...
+   return numbers
+Per provar la funció, fes un programa, al mòdul main.py, que llegeixi una llista d'enters i la imprimeixi per pantalla.
+'''
