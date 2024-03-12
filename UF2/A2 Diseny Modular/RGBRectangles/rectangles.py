fe86c61331aed329445fbed6ad7678a70217f879
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-03-12 10:11:26 +0100

    Proves GIT

--- UF2/A2 Diseny Modular/RGBRectangles/rectangles.py
+++ UF2/A2 Diseny Modular/RGBRectangles/rectangles.py
@@ -1,29 +1,29 @@
 import systemColors
 vermell = systemColors.CRED
 verd = systemColors.CGREEN
-blue = systemColors.CBLUE
+blau = systemColors.CBLUE
 def dades():
     matriu = []
     colors = input()
-    while matriu != ';Q':
-        matriu.append(int(colors))
+    while colors != ';Q':
+        matriu.append(colors)
         colors = input()
     rectangles(matriu)
 
 def rectangles(matriu):
     for colors in matriu:
         colors = colors.lower().split()
-        llargada = colors[1]
-        amplada = colors[2]
-        colors = colors[0]
-        if colors == 'Red':
-            colors = vermell
-        elif colors == 'Verd':
-            colors = verd
-        elif colors == 'Blue':
-            colors = blue
+        llargada = int(colors[1])
+        amplada = int(colors[2])
+        color = colors[0]
+        if color == 'Red':
+            color = vermell
+        elif color == 'Green':
+            color = verd
+        elif color == 'Blue':
+            color = blau
         for i in range(llargada):
             for x in range(amplada):
-                print(colors + 'x', end="")
+                print(color + "X", end="")
             print()
         print()
