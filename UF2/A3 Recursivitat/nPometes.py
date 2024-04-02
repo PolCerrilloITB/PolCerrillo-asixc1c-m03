edd7763c21187a52dd9e1ac0d496578c5bdb020c
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-02 11:33:50 +0200

    update

--- UF2/A3 Recursivitat/nPometes.py
+++ UF2/A3 Recursivitat/nPometes.py
@@ -6,19 +6,13 @@ Descripció:Volem fer un programa que escrigui la cançó de
 N pometes té el pomer (és repeteix fins a que ja no queden pomes)
 '''
 def getAppleSongStanza(applesCount):
-    stanza = f"{applesCount} pometes té el pomer,\n" \
+    if applesCount == 1:
+        word = "pometas"
+    else:
+        word = "pometes"
+    stanza = f"{applesCount} {word} té el pomer,\n" \
              f"de {applesCount} una, de {applesCount} una,\n" \
-             f"{applesCount} pometes té el pomer,\n" \
-             f"de {applesCount} una en caigué.\n" \
-             f"Si mireu el vent d'on vé\n" \
-             f"veureu el pomer com dansa,\n" \
-             f"si mireu el vent d'on vé\n" \
-             f"veureu com dansa el pomer."
-    return stanza
-def getAppleSongStanzaIndividual(applesCount):
-    stanza = f"{applesCount} poma té el pomer,\n" \
-             f"de {applesCount} una, de {applesCount} una,\n" \
-             f"{applesCount} poma té el pomer,\n" \
+             f"{applesCount} {word} té el pomer,\n" \
              f"de {applesCount} una en caigué.\n" \
              f"Si mireu el vent d'on vé\n" \
              f"veureu el pomer com dansa,\n" \
@@ -28,11 +22,10 @@ def getAppleSongStanzaIndividual(applesCount):
 def getAppleSong(applesCount):
     if applesCount == 0:
         return ""
-    elif applesCount == 1:
-        return getAppleSongStanzaIndividual(applesCount) + "\n" +getAppleSong(applesCount-1)
     else:
         return getAppleSongStanza(applesCount) + "\n" +getAppleSong(applesCount -1)
 def main():
-    print(getAppleSong(int(input())))
+    applesCount = int(input())
+    print(getAppleSong(applesCount))
 main()
 
