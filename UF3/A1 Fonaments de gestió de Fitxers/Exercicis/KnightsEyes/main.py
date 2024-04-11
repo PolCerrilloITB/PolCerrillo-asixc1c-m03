1e9c7e08dd495c9256f3b2dcc32d9e99dc3dc7ca
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-11 09:17:08 +0200

    Proves GIT

---
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/KnightsEyes/main.py
@@ -0,0 +1,14 @@
+'''
+Pol Cerrillo
+11/04/2024
+ASIXc1C M03 UF3
+Descripció:Llegir el fitxer Dragon.in i crear el fitxer de sortida DragonEyesClosed.out amb el mateix contingut.
+Però caldrà haver canviar prèviament els ulls del drac que al fitxer d'entrada estan oberts 0    0 , i al de
+sortida hauran d'estar tancats X    X  o també per uns '👁' '👁'
+'''
+import closeKnightsEyes
+
+def main():
+    contingut = closeKnightsEyes.fitxerKnight()
+    closeKnightsEyes.canviulls(contingut)
+main()
