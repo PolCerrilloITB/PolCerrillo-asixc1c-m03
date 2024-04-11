c55fbf0399a66777231aa61bea2f6066aa1c3722
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-11 08:50:28 +0200

    Proves GIT

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/closeDragonEyes.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/closeDragonEyes.py
@@ -1,7 +1,7 @@
 '''
 Pol Cerrillo
-02/04/2023
-ASIXc1C M03 UF1
+11/04/2024
+ASIXc1C M03 UF3
 Descripció:Llegir el fitxer Dragon.in i crear el fitxer de sortida DragonEyesClosed.out amb el mateix contingut.
 Però caldrà haver canviar prèviament els ulls del drac que al fitxer d'entrada estan oberts 0    0 , i al de
 sortida hauran d'estar tancats X    X  o també per uns '👁' '👁'
