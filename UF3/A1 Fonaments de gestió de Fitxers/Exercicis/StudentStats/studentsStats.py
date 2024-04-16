eff3539a64461bbcb3beff517f437109ab683fea
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-16 12:09:33 +0200

    Proves GIT

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/StudentStats/studentsStats.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/StudentStats/studentsStats.py
@@ -8,7 +8,7 @@ Les notes provenen d'un fitxer la ruta del qual serà introduïda per teclat.
 Per tant, la quantitat de notes és indeterminada, i dependrà del contingut de cada fitxer que es faci servir.
 Fes un petit programa que li solucioni la tasca (les notes de l'examen no tenen decimals).
 '''
-
+nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 def llegirFitxer():
     try:
         fitxer = open(input("Ruta del fitxer:"))
@@ -17,6 +17,9 @@ def llegirFitxer():
         return linia
     except:
         print("Ruta incorrecta o no trobada")
+def lletras():
+    if nums in linia:
+        fitxer
 def calculdenotes(notes):
     notes = notes.split()
     notes = [int(nota) for nota in notes]
@@ -27,6 +30,6 @@ def calculdenotes(notes):
 def main():
     notes = llegirFitxer()
     minima, maxima, mitjana = calculdenotes(notes)
-    print(f"Minima: {round(minima, 2)}")
-    print(f"Maxima: {round(maxima, 2)}")
+    print(f"Minima: {round(minima)}")
+    print(f"Maxima: {round(maxima)}")
     print(f"Mitjana: {round(mitjana, 2)}")
