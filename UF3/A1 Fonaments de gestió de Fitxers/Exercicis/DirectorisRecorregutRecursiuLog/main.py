da10d182035a5386f473d0ead59d78bbb68f92e1
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-18 09:12:29 +0200

    Proves GIT

---
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/DirectorisRecorregutRecursiuLog/main.py
@@ -0,0 +1,9 @@
+'''
+Pol Cerrillo
+18/04/2024
+ASIXc1C M03 UF3
+Descripció: Modificar l'exemple Recorregut de directoris recursiu, per a controlat els possibles errors.
+Anotar en un fitxer error.log, els errors trobats (només)
+'''
+import directorisRecorregutRecusiuLog
+directorisRecorregutRecusiuLog.main()
