e909fda63fbe06d3d5b837aa98e4ad34863a2afb
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-18 09:29:57 +0200

    Proves GIT

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/DirectorisRecorregutRecursiuLog/directorisRecorregutRecusiuLog.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/DirectorisRecorregutRecursiuLog/directorisRecorregutRecusiuLog.py
@@ -6,19 +6,23 @@ Descripció: Modificar l'exemple Recorregut de directoris recursiu, per a contro
 Anotar en un fitxer error.log, els errors trobats (només)
 '''
 import os
-def recorrer_arbol_directorios(directorio):
-   # Obtener la lista de archivos y directorios en el directorio actual
-   contenido = os.listdir(directorio)
-   # Recorrer cada elemento del directorio actual
-   for elemento in contenido:
-       # Crear la ruta completa del elemento
-       ruta_elemento = os.path.join(directorio, elemento)
-       # Si el elemento es un directorio, recorrer recursivamente
-       if os.path.isdir(ruta_elemento):
-           print("Directorio:", ruta_elemento)
-           recorrer_arbol_directorios(ruta_elemento)
-       else:
-           print("Archivo:", ruta_elemento)
+def recorrer_arbol_directorios(directorio, archivo_log):
+    try:
+       # Obtener la lista de archivos y directorios en el directorio actual
+       contenido = os.listdir(directorio)
+       # Recorrer cada elemento del directorio actual
+       for elemento in contenido:
+           # Crear la ruta completa del elemento
+           ruta_elemento = os.path.join(directorio, elemento)
+           # Si el elemento es un directorio, recorrer recursivamente
+           if os.path.isdir(ruta_elemento):
+               print("Directorio:", ruta_elemento)
+               recorrer_arbol_directorios(ruta_elemento)
+           else:
+               print("Archivo:", ruta_elemento)
+    except Exception as e:
+        with open(archivo_log, 'a') as log:
+            log.write(f"Error: {str(e)} en el directorio {directorio}\n")
 # Función principal
 def main():
    # Solicitar al usuario el directorio inicial
@@ -29,7 +33,5 @@ def main():
    else:
        # Llamar a la función recursiva para recorrer el árbol de directorios
        print("\nRecorrido del árbol de directorios:")
-       recorrer_arbol_directorios(directorio_inicial)
-if __name__ == "__main__":
-
-   main()
+       archivo_log = "error.log"
+       recorrer_arbol_directorios(directorio_inicial, archivo_log)
