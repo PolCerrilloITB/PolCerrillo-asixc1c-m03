2d83e85463179694ccced906961ca428d34f198d
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-04-18 09:07:23 +0200

    Proves GIT

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/Booksapp/main.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/Booksapp/main.py
@@ -1,3 +1,15 @@
+'''
+Pol Cerrillo
+18/04/2024
+ASIXc1C M03 UF3
+Descripció: Volem fer un petit programa per guardar la informació dels diferents llibres que té una biblioteca.
+A un fitxer s'ha de dir  books.out
+Per introduir els llibres, primer es llegirà els llibres per teclat (en concret: títol, autor, nombre de pàgines).
+El primer nombre indica la quantitat de llibres que es vol introduir.
+En versions futures la lectura dels llibres  es farà d'un fitxer.
+Quan acabi volem que mostri la llista de llibres i el número total, el llibre més curt i el llibre més llarg
+(en cas d'empat el primer que s'hagi llegit del fitxer).
+'''
 import booksApp
 def main():
     llibres = booksApp.guardar_llibres()
