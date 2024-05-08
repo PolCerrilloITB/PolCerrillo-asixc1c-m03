03a629ecf80863dc13f1db492abdda2e50f5c82b
Pol <pol.cerrillo.7e7@itb.cat>
2024-05-08 17:44:55 +0200

    update

---
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/cheapestPrice/cheapestPrice.py
@@ -0,0 +1,17 @@
+'''
+Fes un programa que donada la llista de preus (sense decimals) de tot de productes, retorni el preu del
+producte més baix. La ruta al fitxer serà introduïda per teclat.
+IMPORTANT: no es poden fer servir les funcions de Python.
+'''
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        prices = [int(price) for price in f.read().split()]
+    return prices
+
+
+def find_min_price(prices):
+    min_price = prices[0]
+    for price in prices:
+        if price < min_price:
+            min_price = price
+    return min_price
---
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/cheapestPrice/main.py
@@ -0,0 +1,12 @@
+'''
+Fes un programa que donada la llista de preus (sense decimals) de tot de productes, retorni el preu del
+producte més baix. La ruta al fitxer serà introduïda per teclat.
+IMPORTANT: no es poden fer servir les funcions de Python.
+'''
+import cheapestPrice
+def main():
+    file_path = input("Enter the path to the file with the prices: ")
+    prices = cheapestPrice.read_file(file_path)
+    min_price = cheapestPrice.find_min_price(prices)
+    print(f"El producte més econòmic val: {min_price}€")
+main()
