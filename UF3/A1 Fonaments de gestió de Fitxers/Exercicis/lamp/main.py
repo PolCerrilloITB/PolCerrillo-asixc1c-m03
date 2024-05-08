c61ad87b893435dba0e1630f25396bcdcbe1019a
Pol <pol.cerrillo.7e7@itb.cat>
2024-05-08 17:39:33 +0200

    update

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/lamp/main.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/lamp/main.py
@@ -1,3 +1,14 @@
+'''
+Implementa un programa que simuli un llum, amb les accions:
+TURN ON: Encén el llum
+TURN OFF: Apaga el llum
+TOGGLE: Canvia l'estat del llum
+END: Finalitza la seqüència d'accions. Si el fitxer s'acaba sense tenir aquesta ordre,
+el programa acabarà correctament però indicarà per pantalla que el fitxer no
+acabava amb aquesta acció (serà un "warning" o avís)
+Després de cada acció mostra si el llum està encesa. Les accions vindran en un
+fitxer on cada línia hi haurà només una acció.
+'''
 class Lamp:
     def __init__(self):
         self.is_on = False
