dbd962157824026227f1bc6de102d0db615cf223
Pol <pol.cerrillo.7e7@itb.cat>
2024-05-08 17:45:26 +0200

    update

--- UF3/A1 Fonaments de gestió de Fitxers/Exercicis/cheapestPrice/cheapestPrice.py
+++ UF3/A1 Fonaments de gestió de Fitxers/Exercicis/cheapestPrice/cheapestPrice.py
@@ -3,6 +3,7 @@ Fes un programa que donada la llista de preus (sense decimals) de tot de product
 producte més baix. La ruta al fitxer serà introduïda per teclat.
 IMPORTANT: no es poden fer servir les funcions de Python.
 '''
+file_path = 'precios.txt'
 def read_file(file_path):
     with open(file_path, 'r') as f:
         prices = [int(price) for price in f.read().split()]
