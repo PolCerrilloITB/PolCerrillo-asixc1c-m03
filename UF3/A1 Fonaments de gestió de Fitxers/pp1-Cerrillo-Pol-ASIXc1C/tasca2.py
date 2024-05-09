847b0519296af70e0b3667f7d4145c58597e63fe
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-05-09 09:42:42 +0200

    examen PP3

--- UF3/A1 Fonaments de gestió de Fitxers/pp1-Cerrillo-Pol-ASIXc1C/tasca2.py
+++ UF3/A1 Fonaments de gestió de Fitxers/pp1-Cerrillo-Pol-ASIXc1C/tasca2.py
@@ -4,6 +4,7 @@ Pol Cerrillo
 ASIXc1C M03 UF3
 Descripció: Fer un contador de paraules per cada inicial que hi a a les paraules
 '''
+#Lectura d'inicials
 def llegir_inicials():
     print("Per acabar")
 #Todo fer el fitxer de contador i acabar les definicions
